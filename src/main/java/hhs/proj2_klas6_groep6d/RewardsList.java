82bd8f1e31d56ffcb74f0b7f7581c9b34c8c9f0b
LucaD28 <101333580+LucaD28@users.noreply.github.com>
2022-05-19 15:42:45 +0200

    Rewards
    Co-Authored-By: Joeri <101257742+JoeriGithub@users.noreply.github.com>
    Co-Authored-By: VincentHuijer <94686622+VincentHuijer@users.noreply.github.com>

---
+++ src/main/java/hhs/proj2_klas6_groep6d/RewardsList.java
@@ -0,0 +1,27 @@
+package hhs.proj2_klas6_groep6d;
+
+import java.util.ArrayList;
+
+public class RewardsList {
+
+    public  ArrayList<Rewards> rewardsLijst = new ArrayList<>();
+    public void addRewards(ArrayList<Rewards> toAdd) {
+        for(Rewards reward : toAdd){
+            rewardsLijst.add(reward);
+        }
+    }
+
+    public void removeRewards(int id) { //verdere mogelijkheid zou zijn om een naam mee te geven. Zou voor een gebruiker toepasselijker zijn.
+        for(int i = 0; i < rewardsLijst.size();i++){
+            if(rewardsLijst.get(i).getRewardID() == (id)){
+                rewardsLijst.remove(i);
+            }
+        }
+    }
+
+    public void printRewardLijst() {
+        for (int i = 0; i < rewardsLijst.size(); i++) {
+            System.out.println(rewardsLijst.get(i).getNaam());
+        }
+    }
+}

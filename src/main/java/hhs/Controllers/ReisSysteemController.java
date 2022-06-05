4645299b8b336c1c1f502dc8dc506852fb03e1e8
Siddhart <Siddhart_7@live.nl>
2022-06-05 19:17:06 +0200

    Added CO2 class and a system to change the view
    Co-Authored-By: Mette <47870764+patatjemette@users.noreply.github.com>

--- src/main/java/hhs/Controllers/ReisSysteemController.java
+++ src/main/java/hhs/Controllers/ReisSysteemController.java
@@ -10,6 +10,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
--- src/main/java/hhs/Schermen/ReisSysteemScherm.java
+++ src/main/java/hhs/Schermen/ReisSysteemScherm.java
@@ -1,10 +1,12 @@
 package hhs.Schermen;
 
+import hhs.proj2_klas6_groep6d.CO2;
 import hhs.proj2_klas6_groep6d.Gebruiker;
 import hhs.proj2_klas6_groep6d.Main;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -28,6 +30,13 @@ public class ReisSysteemScherm implements Observer {
         welkomText.setText("Welkom " + loggedIn.getUsername());
         Text punten = (Text) scene.lookup("#puntensaldoText");
         punten.setText(String.format("%.0f PUNTEN", loggedIn.getPunten().getAantalPunten()));
+
+        ChoiceBox viewSwitch = (ChoiceBox) scene.lookup("#viewSwitch");
+
+        viewSwitch.setValue("Punten");
+        viewSwitch.getItems().add("CO2");
+        viewSwitch.getItems().add("Punten");
+
         stage.setTitle("ReisSysteem");
         stage.setScene(scene);
         stage.show();
@@ -53,6 +62,9 @@ public class ReisSysteemScherm implements Observer {
         CheckBox woonWerk = (CheckBox) scene.lookup("#woonwerkCheck");
         CheckBox zakelijk = (CheckBox) scene.lookup("#zakelijkCheck");
         Text puntensaldo = (Text) scene.lookup("#puntensaldoText");
+
+        ChoiceBox choicebox = (ChoiceBox) scene.lookup("#viewSwitch");
+
         puntensaldo.setText(String.format("%.0f PUNTEN", loggedIn.getPunten().getAantalPunten()));
         double km;
         if (textField.getText().equals("")) {
@@ -70,51 +82,66 @@ public class ReisSysteemScherm implements Observer {
             //Hier nog code maken om te bepalen hoe ver van werk de medewerker woont die verst weg woont.
             punten = String.format("%.0f Punten", loggedIn.getPunten().berekenAantalPuntenWoonWerkVerkeer(100, km, arg.toString(), elektrisch));
             text.setText(punten);
-            autoPuntenText.setText(String.format("%.0f Punten", loggedIn.getPunten().berekenAantalPuntenWoonWerkVerkeer(100, km, "Auto", elektrisch)));
-            regionaalPuntenText.setText(String.format("%.0f Punten", loggedIn.getPunten().berekenAantalPuntenWoonWerkVerkeer(100, km, "RegionaalOV", elektrisch)));
-            tramPuntenText.setText(String.format("%.0f Punten", loggedIn.getPunten().berekenAantalPuntenWoonWerkVerkeer(100, km, "Tram", elektrisch)));
-            fietsPuntenText.setText(String.format("%.0f Punten", loggedIn.getPunten().berekenAantalPuntenWoonWerkVerkeer(100, km, "Fiets", elektrisch)));
-            if(arg.toString().equalsIgnoreCase("auto")) {
-                alternatiefWoonWerkVerkeer("regionaalOV", km, elektrisch, arg);
+
+            if (choicebox.getValue().toString().equalsIgnoreCase("punten")) {
+                autoPuntenText.setText(String.format("%.0f Punten", loggedIn.getPunten().berekenAantalPuntenWoonWerkVerkeer(100, km, "Auto", elektrisch)));
+                regionaalPuntenText.setText(String.format("%.0f Punten", loggedIn.getPunten().berekenAantalPuntenWoonWerkVerkeer(100, km, "RegionaalOV", elektrisch)));
+                tramPuntenText.setText(String.format("%.0f Punten", loggedIn.getPunten().berekenAantalPuntenWoonWerkVerkeer(100, km, "Tram", elektrisch)));
+                fietsPuntenText.setText(String.format("%.0f Punten", loggedIn.getPunten().berekenAantalPuntenWoonWerkVerkeer(100, km, "Fiets", elektrisch)));
+            } else {
+                double uitstoot = new CO2(km).getUitstoot();
+                autoPuntenText.setText(String.format("%.0f gram CO2", uitstoot));
+                regionaalPuntenText.setText(String.format("%.0f gram CO2", uitstoot));
+                tramPuntenText.setText(String.format("%.0f gram CO2", uitstoot));
+                fietsPuntenText.setText(String.format("%.0f gram CO2", uitstoot));
             }
 
-            else if(arg.toString().equalsIgnoreCase("RegionaalOV")) {
+            if (arg.toString().equalsIgnoreCase("auto")) {
+                alternatiefWoonWerkVerkeer("regionaalOV", km, elektrisch, arg);
+            } else if (arg.toString().equalsIgnoreCase("RegionaalOV")) {
                 alternatiefWoonWerkVerkeer("tram", km, elektrisch, arg);
 
-            }
-            else if(arg.toString().equalsIgnoreCase("Tram")) {
+            } else if (arg.toString().equalsIgnoreCase("Tram")) {
                 alternatief.setText("");
 
             }
         } else if (zakelijk.isSelected()) {
             punten = String.format("%.0f Punten", loggedIn.getPunten().berekenAantalPuntenZakelijkVerkeer(km, arg.toString(), elektrisch));
             text.setText(punten);
-            autoPuntenText.setText(String.format("%.0f Punten", loggedIn.getPunten().berekenAantalPuntenZakelijkVerkeer(km, "Auto", elektrisch)));
-            regionaalPuntenText.setText(String.format("%.0f Punten", loggedIn.getPunten().berekenAantalPuntenZakelijkVerkeer(km, "RegionaalOV", elektrisch)));
-            tramPuntenText.setText(String.format("%.0f Punten", loggedIn.getPunten().berekenAantalPuntenZakelijkVerkeer(km, "Tram", elektrisch)));
-            fietsPuntenText.setText(String.format("%.0f Punten", loggedIn.getPunten().berekenAantalPuntenZakelijkVerkeer(km, "Fiets", elektrisch)));
 
-            if(arg.toString().equalsIgnoreCase("auto")) {
+            if (choicebox.getValue().toString().equalsIgnoreCase("punten")) {
+                autoPuntenText.setText(String.format("%.0f Punten", loggedIn.getPunten().berekenAantalPuntenZakelijkVerkeer(km, "Auto", elektrisch)));
+                regionaalPuntenText.setText(String.format("%.0f Punten", loggedIn.getPunten().berekenAantalPuntenZakelijkVerkeer(km, "RegionaalOV", elektrisch)));
+                tramPuntenText.setText(String.format("%.0f Punten", loggedIn.getPunten().berekenAantalPuntenZakelijkVerkeer(km, "Tram", elektrisch)));
+                fietsPuntenText.setText(String.format("%.0f Punten", loggedIn.getPunten().berekenAantalPuntenZakelijkVerkeer(km, "Fiets", elektrisch)));
+            } else {
+                double uitstoot = new CO2(km).getUitstoot();
+                autoPuntenText.setText(String.format("%.0f gram CO2", uitstoot));
+                regionaalPuntenText.setText(String.format("%.0f gram CO2", uitstoot));
+                tramPuntenText.setText(String.format("%.0f gram CO2", uitstoot));
+                fietsPuntenText.setText(String.format("%.0f gram CO2", uitstoot));
+            }
+            if (arg.toString().equalsIgnoreCase("auto")) {
                 alternatiefZakelijkVerkeer("regionaalOV", km, elektrisch, arg);
-            }
-            else if(arg.toString().equalsIgnoreCase("RegionaalOV")) {
+            } else if (arg.toString().equalsIgnoreCase("RegionaalOV")) {
                 alternatiefZakelijkVerkeer("tram", km, elektrisch, arg);
-            }
-            else if(arg.toString().equalsIgnoreCase("Tram")) {
+            } else if (arg.toString().equalsIgnoreCase("Tram")) {
                 alternatief.setText("");
             }
         }
     }
-    public void alternatiefWoonWerkVerkeer(String vervoersmiddel, double km, boolean elektrisch, Object arg){ //Zet tekst neer om gebruiker op de hoogte te stellen van een betere optie die meer punten oplevert
+
+    public void alternatiefWoonWerkVerkeer(String vervoersmiddel, double km, boolean elektrisch, Object arg) { //Zet tekst neer om gebruiker op de hoogte te stellen van een betere optie die meer punten oplevert
         Text alternatief = (Text) scene.lookup("#AlternatieveText");
         alternatief.setText("Voor meer punten kunt u met de duurzamere optie gaan want dan krijgt u "
-                + (loggedIn.getPunten().berekenAantalPuntenWoonWerkVerkeer(100,km,vervoersmiddel, elektrisch) - loggedIn.getPunten().berekenAantalPuntenWoonWerkVerkeer(100, km, arg.toString(), elektrisch))
+                + (loggedIn.getPunten().berekenAantalPuntenWoonWerkVerkeer(100, km, vervoersmiddel, elektrisch) - loggedIn.getPunten().berekenAantalPuntenWoonWerkVerkeer(100, km, arg.toString(), elektrisch))
                 + " punten meer.");
     }
-    public void alternatiefZakelijkVerkeer(String vervoersmiddel, double km, boolean elektrisch, Object arg){ //Zet tekst neer om gebruiker op de hoogte te stellen van een betere optie die meer punten oplevert
+
+    public void alternatiefZakelijkVerkeer(String vervoersmiddel, double km, boolean elektrisch, Object arg) { //Zet tekst neer om gebruiker op de hoogte te stellen van een betere optie die meer punten oplevert
         Text alternatief = (Text) scene.lookup("#AlternatieveText");
         alternatief.setText("Voor meer punten kunt u met de duurzamere optie gaan want dan krijgt u "
-                + (loggedIn.getPunten().berekenAantalPuntenZakelijkVerkeer(km,vervoersmiddel, elektrisch) - loggedIn.getPunten().berekenAantalPuntenZakelijkVerkeer(km, arg.toString(), elektrisch))
+                + (loggedIn.getPunten().berekenAantalPuntenZakelijkVerkeer(km, vervoersmiddel, elektrisch) - loggedIn.getPunten().berekenAantalPuntenZakelijkVerkeer(km, arg.toString(), elektrisch))
                 + " punten meer.");
     }
 }

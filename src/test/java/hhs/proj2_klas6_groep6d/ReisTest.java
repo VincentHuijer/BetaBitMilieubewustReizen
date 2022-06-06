a67d090b3e31c3f068e079161bfab07cf0e6c2f2
Helloguis <94686622+VincentHuijer@users.noreply.github.com>
2022-06-06 16:08:17 +0200

    Reis/CO2 Testen gefixed die met aanpassingen niet meer werkten.
    Reis/CO2 Testen gefixed die met aanpassingen niet meer werkten.

--- src/test/java/hhs/proj2_klas6_groep6d/ReisTest.java
+++ src/test/java/hhs/proj2_klas6_groep6d/ReisTest.java
@@ -7,28 +7,30 @@ import java.util.Date;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ReisTest {
-/*
-
     @Test
     void testGetAfstand() {
+        Gebruiker gebruiker = new Gebruiker("testGebruiker", "testWachtwoord", "testVoornaam", "testAchternaam");
         Date date = new Date();
-        Reis reis = new Reis(date,25.4, 132.4,"auto");
+        Reis reis = new Reis(date,25.4, 132.4,"auto", gebruiker);
         assertEquals(132.4, reis.getAfstand());
     }
 
     @Test
     void testGetPunten() {
+        Gebruiker gebruiker = new Gebruiker("testGebruiker", "testWachtwoord", "testVoornaam", "testAchternaam");
+
         Date date = new Date();
-        Reis reis = new Reis(date,25.4, 132.4,"auto");
+        Reis reis = new Reis(date,25.4, 132.4,"auto", gebruiker);
         assertEquals(25.4, reis.getPunten());
     }
 
     @Test
     void testGetVervoersMiddel() {
+        Gebruiker gebruiker = new Gebruiker("testGebruiker", "testWachtwoord", "testVoornaam", "testAchternaam");
         Date date = new Date();
-        Reis reis = new Reis(date,25.4, 132.4,"auto");
+        Reis reis = new Reis(date,25.4, 132.4,"auto", gebruiker);
         assertEquals("auto", reis.getVervoersMiddel());
     }
 
- */
+
 }

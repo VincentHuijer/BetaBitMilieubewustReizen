67956f295ab752e8b5ad457821414f66b749da7a
LucaD28 <101333580+LucaD28@users.noreply.github.com>
2022-06-07 10:19:18 +0200

    BeloningTest
    Co-Authored-By: Joeri <101257742+JoeriGithub@users.noreply.github.com>

--- src/test/java/hhs/proj2_klas6_groep6d/BeloningTest.java
+++ src/test/java/hhs/proj2_klas6_groep6d/BeloningTest.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
 public class BeloningTest {
 
     @Test
-    public void BeloningScherm(){
+    public void testRewardClaimen(){
         BeloningScherm beloningScherm = new BeloningScherm();
         Gebruiker gebruiker = new Gebruiker("test", "test", "test", "test");
         beloningScherm.setLoggedIn(gebruiker);
@@ -20,8 +20,19 @@ public class BeloningTest {
         RewardsList rewards = beloningScherm.getRewardsList();
         ArrayList<Rewards> alleRewards = rewards.getRewardsLijst();
         alleRewards.add(new Rewards("test", "test", 0, 100.0)); // 100 is de prijs in punten.
+        alleRewards.add(new Rewards("test2", "test2", 1, 200.0));
+        Punten puntenGebruiker = gebruiker.getPunten();
         Assertions.assertFalse(controller.claimRewardCheck(1)); //Reward nummer 1
-        gebruiker.getPunten().addPunten(100);
+        Assertions.assertFalse(controller.claimRewardCheck(2)); //Reward nummer 2
+        puntenGebruiker.addPunten(100);
+        Assertions.assertEquals(100, puntenGebruiker.getAantalPunten());
         Assertions.assertTrue(controller.claimRewardCheck(1)); // Reward nummer 1
+        Assertions.assertFalse(controller.claimRewardCheck(2));// Reward nummer 2
+        puntenGebruiker.removePunten(100);
+        Assertions.assertEquals(0, puntenGebruiker.getAantalPunten());
+        puntenGebruiker.addPunten(200);
+        Assertions.assertTrue(controller.claimRewardCheck(1)); // Reward nummer 1
+        Assertions.assertTrue(controller.claimRewardCheck(2)); // Reward nummer 2
+        Assertions.assertEquals(200, puntenGebruiker.getAantalPunten());
     }
 }

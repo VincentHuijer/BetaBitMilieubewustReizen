02f94f3daf45912eb86f8f1de8a74f0d669a99da
patatjemette <patatjemette@gmail.com>
2022-06-08 22:28:40 +0200

    CO2 Test

---
+++ src/test/java/hhs/proj2_klas6_groep6d/CO2Test.java
@@ -0,0 +1,18 @@
+package hhs.proj2_klas6_groep6d;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CO2Test {
+    @Test
+    public void berekenCO2(){
+        CO2 auto = new CO2(10, "auto");
+        CO2 bus = new CO2(10, "RegionaalOV");
+
+        double autoUitstoot = 2240;
+        double busUitstoot = 1160;
+
+        Assertions.assertEquals(auto.getUitstoot(), autoUitstoot);
+        Assertions.assertEquals(bus.getUitstoot(), busUitstoot);
+    }
+
+}

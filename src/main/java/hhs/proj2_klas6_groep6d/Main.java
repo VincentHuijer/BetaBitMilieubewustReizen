9b1968bbfde1ec2f87ef6e44ff5b6b954efe74cd
VincentHuijer <vincent.huijeren@gmail.com>
2022-06-13 10:05:16 +0200

    Proef rewards weggehaald.
    Proef rewards weggehaald. De 2 minuten op het toilet is niet een goede beloning na verdere inzage.

--- src/main/java/hhs/proj2_klas6_groep6d/Main.java
+++ src/main/java/hhs/proj2_klas6_groep6d/Main.java
@@ -13,19 +13,11 @@ public class Main extends Application {//Applicatie kan gestart worden vanaf Log
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
     }//Dit verwijderen in Master
 
     public static void main(String[] args) {
-        RewardsList rewardsList = new RewardsList();
-        ArrayList<Rewards> arrayRewards = new ArrayList<>();
-        arrayRewards.add(new Rewards("toilet", "twee minuten naar het toilet onder werktijd", 1, 4));
-        arrayRewards.add(new Rewards("koffie", "een koffie betaald door werk", 2, 5));
-        arrayRewards.add(new Rewards("uitje", "een uitje betaald door werk", 3, 70));
-        rewardsList.addRewards(arrayRewards);
-        rewardsList.printRewardLijst();
         launch();
     }//Verwijderen in master
 }
